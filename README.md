cc552c124e0cb369de854142150516ca0a4de595
dongweiming <ciici1234@hotmail.com>
2014-12-09 18:27:49 +0800

    Add some magic

---
+++ README.md
@@ -0,0 +1,36 @@
+idb
+===
+
+ipython [db.py](https://github.com/yhat/db.py) shell extension.
+
+Databases Supported
+===
+
+- PostgreSQL
+- MySQL
+- SQLite
+- Redshift
+- MS SQL Server
+- Oracle
+
+Install
+=======
+
+use github:
+
+    In [1]: %install_ext https://raw.githubusercontent.com/dongweiming/idb/master/idb.py
+    In [2]: %load_ext idb.py
+
+    git clone https://github.com/
+    cd idb
+    python setup.py install
+
+use pip:
+
+    pip install idb
+    In [1]: %load_ext idb
+
+Usage
+=============
+
+You can see this example ipynb: http://nbviewer.ipython.org/github/dongweiming/idb/blob/master/examples/db-example.ipynb
--- idb.py
+++ idb.py
@@ -47,6 +47,11 @@ class SQLDB(Magics, Configurable):
                 'dbtype': 'sqlite',
                 'filename': os.path.join(os.path.expanduser('~'), 'db.sqlite')
             }
+        elif uri.scheme == 'sqlite':
+            params = {
+                'dbtype': 'sqlite',
+                'filename': uri.path
+            }
         else:
             params = {
                 'username': get_or_none(uri.username),
@@ -60,6 +65,10 @@ class SQLDB(Magics, Configurable):
 
         return self._db
 
+    @line_magic('db')
+    def db(self, line):
+        return self._db
+
     @line_magic('save_credentials')
     @check_db
     def save_credentials(self, line):
@@ -67,7 +76,7 @@ class SQLDB(Magics, Configurable):
             print '[ERROR]Please Specify credentials name'
         else:
             self._db.save_credentials(profile=line)
-            print 'Save credentials [] successful!'.format(line)
+            print 'Save credentials [] successful!'.format(line[0])
 
     @line_magic('use_credentials')
     @check_db
@@ -77,29 +86,31 @@ class SQLDB(Magics, Configurable):
 
     @line_magic('table')
     @check_db
-    def table(self, *line):
-        l = len(line)
+    def table(self, line):
+        p = line.split()
+        l = len(p)
         if l == 1:
-            if not line[0]:
+            if not p[0]:
                 return self._db.tables
             else:
-                return attrgetter(line[0])(self._db.tables)
+                return attrgetter(p[0])(self._db.tables)
         else:
             data = self._db.tables
-            for p in line:
-                if p in ['head', 'sample', 'unique', 'count', 'all', 'query']:
-                    data = attrgetter(p)(data)()
+            for c in p:
+                if c in ['head', 'sample', 'unique', 'count', 'all', 'query']:
+                    data = attrgetter(c)(data)()
                 else:
-                    data = attrgetter(p)(data)
+                    data = attrgetter(c)(data)
             return data
 
     @line_magic('find_column')
     @check_db
-    def find_column(self, *line):
-        if not len(line):
+    def find_column(self, line):
+        p = line.split()
+        if not line:
             print '[ERROR]Please Specify column wildcard'
         else:
-            return self._db.find_column(*line)
+            return self._db.find_column(*p)
 
     @line_magic('find_table')
     @check_db
@@ -116,6 +127,14 @@ class SQLDB(Magics, Configurable):
             print '[ERROR]Please Specify sql query'
         else:
             return self._db.query(line)
+
+    @line_magic('query')
+    @check_db
+    def query_from_file(self, line):
+        if not line:
+            print '[ERROR]Please Specify sql filepath'
+        else:
+            return self._db.query_from_file(line)
 
 
 def load_ipython_extension(ipython):
