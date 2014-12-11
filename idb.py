c61ae0e41e97938fdc26abea396eb228f3593487
dongweiming <ciici1234@hotmail.com>
2014-12-11 14:00:51 +0800

    use parameter_s

--- idb.py
+++ idb.py
@@ -7,7 +7,6 @@ from urlparse import urlparse
 
 from db import DB
 from IPython.core.magic import Magics, magics_class, line_magic
-from IPython.config.configurable import Configurable
 
 
 def get_or_none(attr):
@@ -25,22 +24,17 @@ def check_db(func):
 
 
 @magics_class
-class SQLDB(Magics, Configurable):
+class SQLDB(Magics):
     _db = None
 
-    def __init__(self, shell):
-        Configurable.__init__(self, config=shell.config)
-        Magics.__init__(self, shell=shell)
-        self.shell.configurables.append(self)
-
     @line_magic('db_connect')
-    def conn(self, line):
+    def conn(self, parameter_s):
         """Conenct to database in ipython shell.
         Examples::
             %db_connect
             %db_connect postgresql://user:pass@localhost:port/database
         """
-        uri = urlparse(line)
+        uri = urlparse(parameter_s)
 
         if not uri.scheme:
             params = {
@@ -66,17 +60,17 @@ class SQLDB(Magics, Configurable):
         return self._db
 
     @line_magic('db')
-    def db(self, line):
+    def db(self, parameter_s):
         return self._db
 
     @line_magic('save_credentials')
     @check_db
-    def save_credentials(self, line):
-        if not line:
+    def save_credentials(self, parameter_s):
+        if not parameter_s:
             print '[ERROR]Please Specify credentials name'
         else:
-            self._db.save_credentials(profile=line)
-            print 'Save credentials [] successful!'.format(line[0])
+            self._db.save_credentials(profile=parameter_s)
+            print 'Save credentials [] successful!'.format(parameter_s[0])
 
     @line_magic('use_credentials')
     @check_db
@@ -86,8 +80,8 @@ class SQLDB(Magics, Configurable):
 
     @line_magic('table')
     @check_db
-    def table(self, line):
-        p = line.split()
+    def table(self, parameter_s):
+        p = parameter_s.split()
         l = len(p)
         if l == 1:
             if not p[0]:
@@ -105,36 +99,38 @@ class SQLDB(Magics, Configurable):
 
     @line_magic('find_column')
     @check_db
-    def find_column(self, line):
-        p = line.split()
-        if not line:
+    def find_column(self, parameter_s):
+        p = parameter_s.split()
+        if not parameter_s:
             print '[ERROR]Please Specify column wildcard'
         else:
             return self._db.find_column(*p)
 
     @line_magic('find_table')
     @check_db
-    def find_table(self, line):
-        if not line:
+    def find_table(self, parameter_s):
+        if not parameter_s:
             print '[ERROR]Please Specify table wildcard'
         else:
-            return self._db.find_table(line)
+            return self._db.find_table(parameter_s)
 
     @line_magic('query')
     @check_db
-    def query(self, line):
-        if not line:
+    def query(self, parameter_s):
+        if not parameter_s:
             print '[ERROR]Please Specify sql query'
         else:
-            return self._db.query(line)
+            return self._db.query(parameter_s)
 
-    @line_magic('query')
+    @line_magic('query_from_file')
     @check_db
-    def query_from_file(self, line):
-        if not line:
+    def query_from_file(self, parameter_s):
+        if not parameter_s:
             print '[ERROR]Please Specify sql filepath'
+        elif not os.path.exists(parameter_s):
+            print '[ERROR]This file {} not exists'.format(parameter_s)
         else:
-            return self._db.query_from_file(line)
+            return self._db.query_from_file(parameter_s)
 
 
 def load_ipython_extension(ipython):
